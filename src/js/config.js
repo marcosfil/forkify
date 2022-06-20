f4510a575659578e2ae5c370f6aa5f0844e846bf
marcosfil <marcosfilho222@gmail.com>
2022-06-20 21:01:07 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = '4b03fc76-bcdf-4096-80cd-97d0712f1ebd';
+
+// export const TIMEOUT_SEC = 10;
